dfdcf03c840db0b4c74aae332212960e6796ff14
James Spence <Jamesaspence20@gmail.com>
2015-11-30 01:35:53 -0600

    code, gitignore, readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # simple-fade
-A library with jquery-like fading ability, written in native Javascript.
+A lightweight, simple, and easy library
+meant to replace the need for jQuery's
+``fadeIn`` and ``fadeOut``.
+
+# Examples
+This library is very lightweight and easy to use.
+Simply require, pass an element and the number of
+milliseconds, and you're finished.
+
+    var simpleFade = require('simple-fade');
+    var element = document.querySelector('#testElement');
+
+To fade out the element, for example:
+
+    simpleFade.fadeOut(3000, element);
+
+To fade it back in:
+
+    simpleFade.fadeIn(3000, element);
+
+simple-fade also supports simple hide / show:
+
+    simpleFade.show(element);
+    simpleFade.hide(element);
---
+++ index.js
@@ -0,0 +1,57 @@
+var inlineStyles = [
+    'b', 'big', 'small', 'i', 'tt', 'abbr', 'acronym', 'cite', 'code', 'dfn', 'em', 'kbd', 'strong',
+    'samp', 'time', 'var', 'a', 'bdo', 'br', 'img', 'map', 'object', 'q', 'script', 'span', 'sub', 'sup',
+    'button', 'input', 'label', 'select', 'textarea'
+];
+
+function getDisplayStyle(elementType) {
+    if (inlineStyles.indexOf(elementType) > -1) {
+        return 'inline';
+    } else {
+        return 'block';
+    }
+}
+
+function fade(type, ms, el) {
+    var isIn = type === 'in',
+        opacity = isIn ? 0 : 1,
+        interval = 50,
+        gap = interval / ms;
+
+    if(isIn) {
+        el.removeAttribute('display');
+        el.style.display = getDisplayStyle(el.tagName.toLowerCase());
+        el.style.opacity = opacity;
+    }
+
+    function func() {
+        opacity = isIn ? opacity + gap : opacity - gap;
+        el.style.opacity = opacity;
+
+        if(opacity <= 0) el.style.display = 'none';
+        if(opacity <= 0 || opacity >= 1) window.clearInterval(fading);
+    }
+
+    var fading = window.setInterval(func, interval);
+
+}
+
+function show(el) {
+    el.style.display = getDisplayStyle(el.tagName.toLowerCase());
+}
+
+function hide(el) {
+    el.style.display = 'none';
+}
+
+module.exports = {
+    fadeOut: function (ms, el) {
+        fade('out', ms, el);
+    },
+    fadeIn: function (ms, el) {
+        fade('in', ms, el);
+    },
+    fade: fade,
+    hide: hide,
+    show: show
+};
